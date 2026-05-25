a066aa04785a1d989c9d2fe864a016cda8918c40
SENTHUR BALAJI K <168910161+senthurbalaji@users.noreply.github.com>
2026-05-25 11:08:49 +0530

    Time: 0 ms (100%), Space: 43 MB (23.77%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkString(String s) {
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)<s.charAt(i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
